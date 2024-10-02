2d42c93414ff7805cbb2e3850147fb2ac5b15546
Allanally <110419895+Allanally@users.noreply.github.com>
2024-10-02 08:51:38 +0200

    ads

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { DocumentsList } from "@/components/documents-list";
 import { Navbar } from "./(dashboard)/_components/navbar";
 
 import { Sidebar } from "./(dashboard)/_components/sidebar";
+import AdRotator from "@/components/AdRotator";
 
 
 const HomePage =  async () => {
@@ -12,19 +13,23 @@ const HomePage =  async () => {
     const documents = await getDocuments();
 
     return (
-      <div className="flex flex-col min-h-screen">
-       <div className="fixed inset-y-0 w-full z-50 h-[80px] md:pl-56">
-        <Navbar />
-      </div>
-      <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-        <Sidebar />
-      </div>
-        
-        <main className="flex-grow md:pl-60 pt-[80px]">
-          <DocumentsList items={documents} />
-        </main>
-      </div>
-    )
-}
+        <div className="flex flex-col min-h-screen">
+          <div className="fixed inset-y-0 w-full z-50 h-[80px] md:pl-56">
+            <Navbar />
+          </div>
+          <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
+            <Sidebar />
+          </div>
+      
+          <main className="flex-grow md:pl-60 pt-[80px]">
+            <DocumentsList items={documents} />
+          </main>
+              
+          <div className="fixed bottom-0 w-full z-40">
+            <AdRotator />
+          </div>
+        </div>
+    );
+  }    
 
 export default HomePage
