2fac20d7d3d9128b95a7fa3c2055b207edf71f4b
Allanally <110419895+Allanally@users.noreply.github.com>
2024-10-07 10:07:58 +0200

    Update footer.jsx

--- app/(dashboard)/_components/footer.jsx
+++ app/(dashboard)/_components/footer.jsx
@@ -4,8 +4,8 @@ import { useState, useEffect } from 'react';
 
 const ads = [
   {
-    image: '/ad1.jpg',
-    phoneNumber: '+250784798564',
+    image: '/screen.png',
+    phoneNumber: '+1234567890',
   },
   {
     image: '/logo.png',
@@ -18,6 +18,7 @@ const ads = [
 ];
 
 const Footer = () => {
+
   const [currentAdIndex, setCurrentAdIndex] = useState(0);
 
   useEffect(() => {
@@ -29,27 +30,27 @@ const Footer = () => {
   }, []);
 
   return (
-    <div className=" w-[90rem] max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+    <footer className="py-4">
       <div className="relative h-40">
-        {ads.map((ad, index) => (
-          <div
-            key={index}
-            className={`absolute inset-0 transition-opacity duration-1000 ${
-              currentAdIndex === index ? 'opacity-100' : 'opacity-0'
-            }`}
-          >
-            <img
-              src={ad.image}
-              alt={`Ad ${index + 1}`}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        ))}
-      </div>
+              {ads.map((ad, index) => (
+                <div
+                  key={index}
+                  className={`absolute inset-0 transition-opacity duration-1000 ${
+                    currentAdIndex === index ? 'opacity-100' : 'opacity-0'
+                  }`}
+                >
+                  <img
+                    src={ad.image}
+                    alt={`Ad ${index + 1}`}
+                    className="w-full h-full object-cover"
+                  />
+                </div>
+              ))}
+            </div>
       <div className="p-4 text-center">
         <p className="text-lg font-semibold">Call Us: {ads[currentAdIndex].phoneNumber}</p>
       </div>
-    </div>
+    </footer>
   );
 };
 
